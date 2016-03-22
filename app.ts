d2f3a63d838527a8c1d40ca3fa705ae11e1d582c
Alexandre Roba <alexandre.roba@arobanet.be>
2016-03-22 21:06:10 +0100

    Added TsLint and an initial configuration

--- app.ts
+++ app.ts
@@ -1,21 +1,21 @@
-import * as express from 'express';
+import * as express from "express";
 
 let app = express();
 
 let port = 3000;
 
-app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.use(express.static("public"));
+app.use(express.static("src/views"));
 
-app.get('/', (req: express.Request,res: express.Response)=>{
-   res.send(200,'Hello World');
+app.get("/", (req:express.Request, res:express.Response)=> {
+    res.send(200, "Hello World");
 });
 
-app.get('/books', (req: express.Request,res: express.Response)=>{
-   res.send(200,'Hello World');
+app.get("/books", (req:express.Request, res:express.Response)=> {
+    res.send(200, "Hello World");
 });
 
-app.listen(port,()=>{
+app.listen(port, ()=> {
     console.log(`running server on port ${port}`);
 });
 
