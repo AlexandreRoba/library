20f936a52cfcca1afa3a4cef03a6ecb6a3098704
Alexandre Roba <alexandre.roba@arobanet.be>
2016-03-22 20:10:54 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+
+let app = express();
+
+let port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req: express.Request,res: express.Response)=>{
+   res.send(200,'Hello World');
+});
+
+app.get('/books', (req: express.Request,res: express.Response)=>{
+   res.send(200,'Hello World');
+});
+
+app.listen(port,()=>{
+    console.log(`running server on port ${port}`);
+});
+
+
+
